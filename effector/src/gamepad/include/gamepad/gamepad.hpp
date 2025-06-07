1952f2ace06cc5ff753b3eae7a0c83e7769a7185
Raegan Scheet <camscheet@gmail.com>
2025-06-07 19:03:37 -0400

    controller commit

--- effector/src/gamepad/include/gamepad/gamepad.hpp
+++ effector/src/gamepad/include/gamepad/gamepad.hpp
@@ -4,6 +4,7 @@
 #include <libevdev/libevdev.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <linux/joystick.h>
 
 #include "hardware_interface/system_interface.hpp"
 #include "hardware_interface/types/hardware_interface_return_values.hpp"
@@ -21,8 +22,11 @@ public:
   hardware_interface::return_type read(const rclcpp::Time & time, const rclcpp::Duration & period) override;
 
 private:
+  std::string device_path_;
+  int fd_ = -1;
+  std::vector<double> axes_;
+  std::vector<double> buttons_;
 };
-
 
 }  // namespace gamepad
 
--- effector/src/gamepad/src/gamepad.cpp
+++ effector/src/gamepad/src/gamepad.cpp
@@ -6,17 +6,52 @@ namespace gamepad
 
 hardware_interface::CallbackReturn GamepadHardware::on_init(const hardware_interface::HardwareInfo & info)
 {
+  if (hardware_interface::SystemInterface::on_init(info) != CallbackReturn::SUCCESS)
+    return CallbackReturn::ERROR;
 
+  // store and open device path from config
+  device_path_ = info_.hardware_parameters.at("device");
+  fd_ = open(device_path_.c_str(), O_RDONLY | O_NONBLOCK);
+
+  if (fd_ < 0) 
+  {
+    RCLCPP_ERROR(rclcpp::get_logger("GamepadHardware"), "Failed to open device: %s", device_path_.c_str());
+    return CallbackReturn::ERROR;
+  }
+
+  // allocate space for axes/buttons
+  axes_.resize(4, 0.0);
+  buttons_.resize(12, 0.0);
+
+  return CallbackReturn::SUCCESS;
 }
 
 std::vector<hardware_interface::StateInterface> GamepadHardware::export_state_interfaces()
 {
+  std::vector<hardware_interface::StateInterface> state_interfaces;
 
+  for (size_t i = 0; i < axes_.size(); ++i)
+    state_interfaces.emplace_back("axis_" + std::to_string(i), hardware_interface::HW_IF_POSITION, &axes_[i]);
+  
+  for (size_t i = 0; i < buttons_.size(); ++i)
+    state_interfaces.emplace_back("button_" + std::to_string(i), hardware_interface::HW_IF_POSITION, &buttons_[i]);
+
+  return state_interfaces;
 }
 
 hardware_interface::return_type GamepadHardware::read(const rclcpp::Time & time, const rclcpp::Duration & period)
 {
+  struct js_event event;
+  while (::read(fd_, &event, sizeof(event)) > 0) {
+    event.type &= ~JS_EVENT_INIT;  // Ignore init events
 
+    if (event.type == JS_EVENT_AXIS && event.number < axes_.size()) 
+      axes_[event.number] = static_cast<double>(event.value) / 32767.0;
+
+    else if (event.type == JS_EVENT_BUTTON && event.number < buttons_.size())
+      buttons_[event.number] = static_cast<double>(event.value);
+  }
+  return hardware_interface::return_type::OK;
 }
 
 }  // namespace gamepad
