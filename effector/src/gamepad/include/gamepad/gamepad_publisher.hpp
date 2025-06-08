fd8263de52f3ed79525130cf8bd30dd090034fb6
Raegan Scheet <camscheet@gmail.com>
2025-06-08 17:00:00 -0400

    publisher  now publshes

--- effector/src/gamepad/include/gamepad/gamepad_publisher.hpp
+++ effector/src/gamepad/include/gamepad/gamepad_publisher.hpp
@@ -1,32 +1,35 @@
+#ifndef _GAMEPAD_CONTROLLER_HPP
+#define _GAMEPAD_CONTROLLER_HPP
 
-#ifndef GAMEPAD_PUBLISHER_HPP
-#define GAMEPAD_PUBLISHER_HPP
-
-#include <controller_interface/controller_interface.hpp>
-#include <sensor_msgs/msg/joy.hpp>
-#include <rclcpp_lifecycle/lifecycle_publisher.hpp>
+#include "controller_interface/controller_interface.hpp"
+#include "sensor_msgs/msg/joy.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_lifecycle/state.hpp"
 
 namespace gamepad
 {
+
 class GamepadPublisher : public controller_interface::ControllerInterface
 {
 public:
   controller_interface::CallbackReturn on_init() override;
-  controller_interface::CallbackReturn on_configure(const rclcpp_lifecycle::State & previous_state) override;
-
-  controller_interface::CallbackReturn on_activate(const rclcpp_lifecycle::State & previous_state) override;
-  controller_interface::CallbackReturn on_deactivate(const rclcpp_lifecycle::State & previous_state) override;
-
+  
   controller_interface::InterfaceConfiguration command_interface_configuration() const override;
+  
   controller_interface::InterfaceConfiguration state_interface_configuration() const override;
-
+  
+  controller_interface::CallbackReturn on_configure(const rclcpp_lifecycle::State & previous_state) override;
+  
+  controller_interface::CallbackReturn on_activate(const rclcpp_lifecycle::State & previous_state) override;
+  
+  controller_interface::CallbackReturn on_deactivate(const rclcpp_lifecycle::State & previous_state) override;
+  
   controller_interface::return_type update(const rclcpp::Time & time, const rclcpp::Duration & period) override;
 
 private:
-  std::shared_ptr<rclcpp_lifecycle::LifecyclePublisher<sensor_msgs::msg::Joy>> joy_pub_;
-  std::vector<std::reference_wrapper<const hardware_interface::LoanedStateInterface>> axis_states_;
-  std::vector<std::reference_wrapper<const hardware_interface::LoanedStateInterface>> button_states_;
+  rclcpp::Publisher<sensor_msgs::msg::Joy>::SharedPtr joy_publisher_;
 };
-} // namespace gamepad
 
-#endif // GAMEPAD_PUBLISHER_HPP
+} // namespace gamepad_controller
+
+#endif // _GAMEPAD_CONTROLLER_HPP
--- effector/src/gamepad/src/gamepad_publisher.cpp
+++ effector/src/gamepad/src/gamepad_publisher.cpp
@@ -1,124 +1,85 @@
 #include "gamepad/gamepad_publisher.hpp"
+#include "sensor_msgs/msg/joy.hpp"
+#include "rclcpp/rclcpp.hpp"
 
 namespace gamepad
 {
 
-controller_interface::CallbackReturn GamepadPublisher::on_activate(const rclcpp_lifecycle::State & /*previous_state*/)
-{
-  joy_pub_->on_activate();
-  RCLCPP_INFO(get_node()->get_logger(), "GamepadPublisher activated");
-  return controller_interface::CallbackReturn::SUCCESS;
-}
-
-controller_interface::CallbackReturn GamepadPublisher::on_deactivate(const rclcpp_lifecycle::State & /*previous_state*/)
-{
-  joy_pub_->on_deactivate();
-  RCLCPP_INFO(get_node()->get_logger(), "GamepadPublisher deactivated");
-  return controller_interface::CallbackReturn::SUCCESS;
-}
-
 controller_interface::CallbackReturn GamepadPublisher::on_init()
 {
-  joy_pub_ = get_node()->create_publisher<sensor_msgs::msg::Joy>("joy", 10);
-  return controller_interface::CallbackReturn::SUCCESS;
-}
-
-controller_interface::CallbackReturn GamepadPublisher::on_configure(const rclcpp_lifecycle::State &)
-{
-  axis_states_.clear();
-  button_states_.clear();
-
-  RCLCPP_INFO(get_node()->get_logger(), "Configuring GamepadPublisher with %zu state interfaces", state_interfaces_.size());
-  
-  for (const auto & state : state_interfaces_)
-  {
-    RCLCPP_INFO(get_node()->get_logger(), "Found interface %s/%s", state.get_name().c_str(), state.get_interface_name().c_str());
-    if (state.get_interface_name() == "position")
-    {
-      if (state.get_name().rfind("axis_", 0) == 0)
-      {
-        axis_states_.push_back(std::cref(state));
-        RCLCPP_INFO(get_node()->get_logger(), "Added axis interface: %s", state.get_name().c_str());
-
-      }
-      else if (state.get_name().rfind("button_", 0) == 0)
-      {
-        button_states_.push_back(std::cref(state));
-        RCLCPP_INFO(get_node()->get_logger(), "Added button interface: %s", state.get_name().c_str());
-      }
-    }
-  }
-  RCLCPP_INFO(get_node()->get_logger(), "Configured with %zu axes and %zu buttons", axis_states_.size(), button_states_.size());
   return controller_interface::CallbackReturn::SUCCESS;
 }
 
 controller_interface::InterfaceConfiguration GamepadPublisher::command_interface_configuration() const
 {
-  return {controller_interface::interface_configuration_type::NONE, {}};
+  controller_interface::InterfaceConfiguration config;
+  config.type = controller_interface::interface_configuration_type::NONE;
+  return config;
 }
 
 controller_interface::InterfaceConfiguration GamepadPublisher::state_interface_configuration() const
 {
-  std::vector<std::string> interface_names;
+  controller_interface::InterfaceConfiguration config;
+  config.type = controller_interface::interface_configuration_type::INDIVIDUAL;
   
-  // Add the axes you want to subscribe to
-  interface_names.push_back("axis_0/position");
-  interface_names.push_back("axis_1/position");
-  interface_names.push_back("axis_2/position");
-  interface_names.push_back("axis_3/position");
+  // Add all axis and button interfaces
+  for (int i = 0; i < 4; ++i) {
+    config.names.push_back("axis_" + std::to_string(i) + "/position");
+  }
+  for (int i = 0; i < 4; ++i) {
+    config.names.push_back("button_" + std::to_string(i) + "/position");
+  }
   
-  // Add the buttons you want to subscribe to
-  interface_names.push_back("button_0/position");
-  interface_names.push_back("button_1/position");
-  interface_names.push_back("button_2/position");
-  interface_names.push_back("button_3/position");
-  
-  return {
-    controller_interface::interface_configuration_type::INDIVIDUAL, interface_names};
+  return config;
 }
 
-controller_interface::return_type GamepadPublisher::update(const rclcpp::Time &, const rclcpp::Duration &)
+controller_interface::CallbackReturn GamepadPublisher::on_configure(const rclcpp_lifecycle::State & /*previous_state*/)
 {
-  sensor_msgs::msg::Joy joy_msg;
-  joy_msg.header.stamp = get_node()->get_clock()->now();
-  joy_msg.axes.reserve(axis_states_.size());
-  joy_msg.buttons.reserve(button_states_.size());
+  // Create publisher
+  joy_publisher_ = get_node()->create_publisher<sensor_msgs::msg::Joy>("joy", 10);
   
-  for (const auto & axis : axis_states_)
-  {
-    auto value = axis.get().get_optional();
-    // joy_msg.axes.push_back(static_cast<float>(value.value_or(0.0)));
-    if (value.has_value())
-    {
-      joy_msg.axes.push_back(static_cast<float>(value.value()));
-      RCLCPP_INFO(get_node()->get_logger(), "Axis %s: %f", axis.get().get_name().c_str(), value.value());
-    } 
-    else {
-      joy_msg.axes.push_back(0.0f);
-      RCLCPP_WARN(get_node()->get_logger(), "Axis %s has no value", axis.get().get_name().c_str());
-    }
+  RCLCPP_INFO(get_node()->get_logger(), "Gamepad controller configured");
+  return controller_interface::CallbackReturn::SUCCESS;
+}
+
+controller_interface::CallbackReturn GamepadPublisher::on_activate(const rclcpp_lifecycle::State & /*previous_state*/)
+{
+  RCLCPP_INFO(get_node()->get_logger(), "Gamepad controller activated");
+  return controller_interface::CallbackReturn::SUCCESS;
+}
+
+controller_interface::CallbackReturn GamepadPublisher::on_deactivate(const rclcpp_lifecycle::State & /*previous_state*/)
+{
+  RCLCPP_INFO(get_node()->get_logger(), "Gamepad controller deactivated");
+  return controller_interface::CallbackReturn::SUCCESS;
+}
+
+controller_interface::return_type GamepadPublisher::update(const rclcpp::Time & time, const rclcpp::Duration & /*period*/)
+{
+  // Create Joy message
+  auto joy_msg = std::make_unique<sensor_msgs::msg::Joy>();
+  joy_msg->header.stamp = time;
+  joy_msg->header.frame_id = "gamepad";
+  
+  // Read axes
+  joy_msg->axes.resize(4);
+  for (int i = 0; i < 4; ++i) {
+    joy_msg->axes[i] = state_interfaces_[i].get_value();
   }
-    
-  for (const auto & button : button_states_)
-  {
-    auto value = button.get().get_optional();
-    // joy_msg.buttons.push_back(static_cast<int32_t>(value.value_or(0.0)));
-    if (value.has_value())
-    {
-      joy_msg.buttons.push_back(static_cast<int32_t>(value.value()));
-      RCLCPP_INFO(get_node()->get_logger(), "Button %s: %f", button.get().get_name().c_str(), value.value());
-    }
-    else {
-      joy_msg.buttons.push_back(0);
-      RCLCPP_WARN(get_node()->get_logger(), "Button %s has no value", button.get().get_name().c_str());
-    }
+  
+  // Read buttons
+  joy_msg->buttons.resize(4);
+  for (int i = 0; i < 4; ++i) {
+    joy_msg->buttons[i] = static_cast<int32_t>(state_interfaces_[i + 4].get_value());
   }
-  RCLCPP_INFO(get_node()->get_logger(), "Publishing Joy msg with %zu axes and %zu buttons", joy_msg.axes.size(), joy_msg.buttons.size());
-  joy_pub_->publish(joy_msg);
+  
+  // Publish the message
+  joy_publisher_->publish(std::move(joy_msg));
+  
   return controller_interface::return_type::OK;
 }
 
-} // namespace gamepad
+} // namespace gamepad_controller
 
 #include "pluginlib/class_list_macros.hpp"
 PLUGINLIB_EXPORT_CLASS(gamepad::GamepadPublisher, controller_interface::ControllerInterface)
